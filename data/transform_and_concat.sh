a01566994df2c6326db96d1bea99fb0340c7c15a
Francisco Rente <up201906761@up.pt>
2023-04-10 23:01:08 +0100

    correct jq concat

--- data/transform_and_concat.sh
+++ data/transform_and_concat.sh
@@ -1,17 +1,23 @@
 #!/bin/zsh
 
-# FINAL_FILE_NAME="products.json"
-# 
-# data_dir=./datasets/source
-# 
-# for entry in "$data_dir"/*
-# do
-#   FILE_NAME=$(basename "$entry")
-#   python3 transform.py $FILE_NAME
-# done
+FINAL_FILE_NAME="products.json"
+ 
+data_dir=./datasets/source
 
-FILES=./datasets/json/*.json
-echo $FILES
-#jq ".[]" -s $FILES > datasets/json/$FINAL_FILE_NAME
+for entry in "$data_dir"/*
+do
+  FILE_NAME=$(basename "$entry")
+  python3 transform.py $FILE_NAME
+done
+
+OUTPUT=./datasets/json
+if [ -f "$OUTPUT/$FINAL_FILE_NAME" ]; then
+  rm "$OUTPUT/$FINAL_FILE_NAME"
+fi
+
+files=`ls $OUTPUT/*.json`  # | tr
+echo $files
+print $files | xargs jq ".[]" -s > "datasets/json/$FINAL_FILE_NAME"
+
 
 
