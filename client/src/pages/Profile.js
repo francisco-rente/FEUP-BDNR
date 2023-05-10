b0c40cca1204f2ec66fdcd0f22b7f2a791b2eb36
Francisco Rente <up201906761@up.pt>
2023-05-10 21:24:01 +0100

    connected profile to backend endpoint

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -6,88 +6,121 @@ import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
 import Typography from "@material-ui/core/Typography";
+import { useParams } from "react-router-dom";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    padding: theme.spacing(4),
-  },
-  avatar: {
-    width: theme.spacing(12),
-    height: theme.spacing(12),
-    margin: theme.spacing(2),
-  },
-  reviewItem: {
-    borderBottom: `1px solid ${theme.palette.grey[300]}`,
-  },
-  reviewTitle: {
-    fontWeight: "bold",
-  },
-  reviewBody: {
-    marginTop: theme.spacing(1),
-    marginBottom: theme.spacing(2),
-  },
+    root: {
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        padding: theme.spacing(4),
+    },
+    avatar: {
+        width: theme.spacing(12),
+        height: theme.spacing(12),
+        margin: theme.spacing(2),
+    },
+    reviewItem: {
+        borderBottom: `1px solid ${theme.palette.grey[300]}`,
+    },
+    reviewTitle: {
+        fontWeight: "bold",
+    },
+    reviewBody: {
+        marginTop: theme.spacing(1),
+        marginBottom: theme.spacing(2),
+    },
 }));
 
+
+
+
+
+
 function Profile() {
-  const classes = useStyles();
+    const classes = useStyles();
+    const [user, setUser] = React.useState(null);
+    const useparam = useParams();
+    const navigate = useNavigate();
+    const user_id = useparam.id;
 
-  // Replace with real user data
-  const user = {
-    name: "John Doe",
-    email: "johndoe@example.com",
-    avatarUrl: "https://i.pravatar.cc/300",
-    reviews: [
-      {
-        title: "Great product",
-        body: "I love this product! It works great and looks stylish.",
-        date: "2023-04-30",
-      },
-      {
-        title: "Could be better",
-        body: "This product is okay, but could be improved in some areas.",
-        date: "2023-04-28",
-      },
-    ],
-  };
+    useEffect( () => {
+        
+        const userId = user_id ? user_id : localStorage.getItem("userId");
+        if (!userId) navigate("/login");
+        console.log("Sending request to get user with id: " + userId);
 
-  return (
-    <>
-      <MyNavbar hasSearchBar={false} style={{ height: "50px" }}></MyNavbar>
-      <div className={classes.root}>
-        <Avatar
-          alt={user.name}
-          src={user.avatarUrl}
-          className={classes.avatar}
-        />
-        <Typography variant="h4">{user.name}</Typography>
-        <Typography variant="subtitle1">{user.email}</Typography>
-        <List>
-          {user.reviews.map((review) => (
-            <ListItem key={review.date} className={classes.reviewItem}>
-              <ListItemText
-                primary={
-                  <Typography
-                    variant="subtitle1"
-                    className={classes.reviewTitle}
-                  >
-                    {review.title}
-                  </Typography>
-                }
-                secondary={
-                  <Typography variant="body2" className={classes.reviewBody}>
-                    {review.body}
-                  </Typography>
-                }
-              />
-            </ListItem>
-          ))}
-        </List>
-      </div>
-    </>
-  );
+
+        async function fetchUser(userId) {
+            
+            console.log("Calling fetchUser");
+            await fetch("http://localhost:3001/api/customer/" + userId).then(
+                (response) => response.json()
+            ).then((data) => {
+                if(data.error) navigate("/login");
+                const reviews = data.reviews.map((review) => {
+                    return {
+                        product_title: review.product_title,
+                        productId: review.product_id,
+                        body: review.r.review_body,
+                        date: review.r.review_date,
+                        title: review.r.review_headline,
+                    }
+                }); 
+                console.log("reviews", reviews);
+                setUser({
+                    name: data.customer.name,
+                    email: data.customer.email,
+                    reviews: reviews,
+                })
+            }
+            ).catch((err) => {
+                console.log("Error: " + err);
+                return null;
+            })
+        }
+        fetchUser(userId);
+    }, []);
+
+    if (!user) return null;// TODO: Fix this Suspense or loading here
+
+    return (
+        <>
+            <MyNavbar hasSearchBar={false} style={{ height: "50px" }}></MyNavbar>
+            <div className={classes.root}>
+                <Avatar
+                    alt={user.name}
+                    src={user.avatarUrl}
+                    className={classes.avatar}
+                />
+                <Typography variant="h4">{user.name}</Typography>
+                <Typography variant="subtitle1">{user.email}</Typography>
+                <List>
+                    {user.reviews.map((review) => (
+                        <ListItem key={review.date} className={classes.reviewItem}>
+                            <ListItemText
+                                primary={
+                                    <Typography
+                                        variant="subtitle1"
+                                        className={classes.reviewTitle}
+                                    >
+                                        {review.title}
+                                    </Typography>
+                                }
+                                secondary={
+                                    <Typography variant="body2" className={classes.reviewBody}>
+                                        {review.body}
+                                    </Typography>
+                                }
+                            />
+                        </ListItem>
+                    ))}
+                </List>
+            </div>
+        </>
+    );
 }
 
 export default Profile;
--- server/controllers/customer.js
+++ server/controllers/customer.js
@@ -1,45 +1,59 @@
 const Customer = require('../models/customer');
-
 const db = require("../db/database");
 
 
 
 const customerController = {
-  getAll: async (req, res, next) => {
-    try {
-      const customer = await Customer.findAll();
-      res.json(customer);
-    } catch (err) {
-      next(err);
-    }
-  },
+    getAll: async (req, res, next) => {
+        try {
+            const customer = await Customer.findAll();
+            res.json(customer);
+        } catch (err) {
+            next(err);
+        }
+    },
 
-  getById: async (req, res, next) => {
-    try {
-      const customer = await Customer.findById(req.params.id);
-      if (!customer) {
-        res.status(404).json({ message: 'Store not found' });
-      } else {
-        res.json(Store);
-      }
-    } catch (err) {
-      next(err);
-    }
-  },
+    getById: async (req, res, next) => {
+        try {      
+            
+            // TODO: collect these queries in a JOIN 
+            // https://docs.couchbase.com/server/current/n1ql/n1ql-language-reference/join.html#ansi-join-with-arrays-on-both-sides
+            
+            const customer = await Customer.findById(parseInt(req.params.id));
+            if (!customer) res.status(404).json({ message: 'Customer not found' });
 
+            const reviews = customer.content.products_reviews_pairs.map((pair) => {
+                return pair.review_id;
+            })
+            
+            const query = `SELECT p.product_id, p.product_title,r FROM server.store.products AS p UNNEST p.reviews AS r WHERE r.review_id IN ${JSON.stringify(reviews)}`;
+            const scope = db.getScope(); 
+            const data = await scope.query(query, (err, result) => err ? err : result);
+            if (data.error) res.status(404).json({ message: 'Customer not found' });
+            console.log("data", data);
+            
+            const response = {
+                customer: customer.content,
+                reviews: data.rows
+            }
 
-  login: async (req, res, next) => {
-    try {
-        const userId = req.body.userId;
-        const customer = await Customer.findById(userId);
-        if (!customer)  res.status(404).json({ message: 'Customer not found' });
-        res.json(customer);
+            res.status(200).json(response);
+        } catch (err) {
+            next(err);
+        }
+    },
+    login: async (req, res, next) => {
+        try {
+            const userId = req.body.userId;
+            const customer = await Customer.findById(userId);
+            if (!customer)  res.status(404).json({ message: 'Customer not found' });
+            res.json(customer);
+        }
+        catch (err) {
+            next(err);
+        }
     }
-    catch (err) {
-        next(err);
-    }
-  }
-/*
+    /*
   create: async (req, res, next) => {
     try {
       const customer = await Store.create(req.body);
@@ -70,7 +84,7 @@ const customerController = {
         res.sendStatus(204);
       } else {
         res.status(404).json({ message: 'store not found' });
-      
+
     } catch (err) {
       next(err);
     }
