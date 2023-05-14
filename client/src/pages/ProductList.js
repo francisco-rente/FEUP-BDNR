4c6ae41cd30b388063c05a562c15dc689d9d3f56
Pedro Pinheiro <pedro.s.pinheiro2001@gmail.com>
2023-05-14 21:17:12 +0100

    small improvement when no reviews are found

--- client/src/pages/ProductList.js
+++ client/src/pages/ProductList.js
@@ -129,9 +129,16 @@ const ProductList = () => {
         await fetch(url + "?" + params)
             .then((res) => res.json())
             .then((data) => {
+                if(data.length == 0) {
+                    console.log("No results found");
+                    setSearchResults([]);
+                    setTotalPages(1);
+                }
+                else{
                 console.log("data from queryProducts", data)
                 setSearchResults(data.rows);
                 setTotalPages(+data.total);
+                }
             })
             .catch((err) => console.log(err));
     }
